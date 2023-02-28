ed85efe0e3f2f2a8ba9a04305396bf612c92acfe
Malu Rezende <maluar@gmail.com>
2023-02-28 14:17:39 -0300

    feat: add remove participants functionality

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -26,10 +26,11 @@ export function Home() {
   }
 
   function handleParticipantRemove(name: string) {
+
     Alert.alert("Remover", `Remover participante ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado!")
+        onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
       },
       {
         text: "Não",
